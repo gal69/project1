d18da49aa993df34ab7e8249a70813a23fd06636
smanz1 <52636326+smanz1@users.noreply.github.com>
2019-07-28 18:28:13 -0400

    Update Cell.java

--- AIproject1/Cell.java
+++ AIproject1/Cell.java
@@ -1,4 +1,25 @@
+//slightly changed but should still be good as there's two constructors.
+//the old pre-edit cell object class is commented out below
 public class Cell {
+	double terrain;
+	double probOfFinding;
+	boolean target;
+	int x;
+	int y;
+	int timesSearched;
+	public Cell(double c, double prob, int i, int j, boolean temp) {
+		terrain=c;
+		probOfFinding=prob;
+		x=i;
+		y=j;
+		target=temp;
+	}
+	public Cell(double c, double prob) {
+		terrain=c;
+		probOfFinding=prob;
+	}
+}
+/*public class Cell {
 	
 	double terrain;
 	double probOfFinding;
@@ -13,4 +34,4 @@ public class Cell {
 	}
 	
 	
-}
+}*/
