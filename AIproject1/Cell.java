6912d787be38209e8c43f60558132cd828dc013f
smanz1 <52636326+smanz1@users.noreply.github.com>
2019-07-28 00:04:03 -0400

    Create Cell.java

---
+++ AIproject1/Cell.java
@@ -0,0 +1,16 @@
+public class Cell {
+	
+	double terrain;
+	double probOfFinding;
+	boolean target;
+	int x;
+	int y;
+	
+	public Cell(double c, double prob) {
+		terrain=c;
+		probOfFinding=prob;
+		
+	}
+	
+	
+}
